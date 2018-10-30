53b088c629926d24475bfd3ac10ec53df0520fad
samzhuwj <samzhuwj@gmail.com>
2018-10-30 18:39:08 +0800

    convert guess type from string to u32

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ fn main() {
     println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
